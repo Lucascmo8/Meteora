3c636515180cc690feb9ee722ae11f878096f476
Lucas Costa <103662624+Lucascmo8@users.noreply.github.com>
2023-08-10 19:47:16 -0300

    Mudança geral no carrossel
    Novidades:
    - Uso do Transition para fazer a animação
    - Animação de voltar e Animação de próximo
    - Tempo de espera para ativar o carrossel novamente
    
    Mudanças:
    - Usando o setInterval para fazer o carrossel
    - Usando o handleVisibilityChange para parar as animações quando o site não está visível

--- src/components/CarouselImages.vue
+++ src/components/CarouselImages.vue
@@ -1,37 +1,44 @@
 <template>
     <div id="carousel" @mouseenter="stopCarousel" @mouseleave="startCarousel">
         <div id="carouselInner">
-            <picture>
-                <source media="(max-width: 548px)" srcset="../assets/Mobile/BannerCarousel1.png">
-                <source media="(max-width: 900px)" srcset="../assets/Tablet/BannerCarousel1.png">
-                <img src="../assets/Desktop/BannerCarousel1.png" alt=""  v-show="indexImg == 0" class="animate__animated animate__slideInRight"></picture>
-            <picture>
-                <source media="(max-width: 548px)" srcset="../assets/Mobile/BannerCarousel2.png">
-                <source media="(max-width: 900px)" srcset="../assets/Tablet/BannerCarousel2.png">
-                <img src="../assets/Desktop/BannerCarousel2.png" alt="" v-show="indexImg == 1" class="animate__animated animate__slideInRight">
-                <figcaption v-show="indexImg == 1" class="animate__animated animate__slideInRight" id="captionBanner2">
-                    <h1>COLEÇÃO ATEMPORAL</h1>
-                    <p>Estilo e qualidade para durar.</p>
-                </figcaption>
-            </picture>
-            <picture>
-                <source media="(max-width: 548px)" srcset="../assets/Mobile/BannerCarousel3.png">
-                <source media="(max-width: 900px)" srcset="../assets/Tablet/BannerCarousel3.png">
-                <img src="../assets/Desktop/BannerCarousel3.png" alt="" v-show="indexImg == 2" class="animate__animated animate__slideInRight">
-                <figcaption v-show="indexImg == 2" class="animate__animated animate__slideInRight" id="captionBanner3">
-                    <h1>COLEÇÃO ATEMPORAL</h1>
-                    <p>Alto impacto visual, baixo impacto ambiental!</p>
-                </figcaption>
-            </picture>
+            <Transition :name="animateName">
+                <picture v-if="indexImg == 0">
+                    <source media="(max-width: 548px)" srcset="../assets/Mobile/BannerCarousel1.png">
+                    <source media="(max-width: 900px)" srcset="../assets/Tablet/BannerCarousel1.png">
+                    <img src="../assets/Desktop/BannerCarousel1.png" alt="Banner Principal">
+                </picture>
+            </Transition>
+            <Transition :name="animateName">
+                <picture v-show="indexImg == 1">
+                    <source media="(max-width: 548px)" srcset="../assets/Mobile/BannerCarousel2.png">
+                    <source media="(max-width: 900px)" srcset="../assets/Tablet/BannerCarousel2.png">
+                    <img src="../assets/Desktop/BannerCarousel2.png" alt="Segundo banner" >
+                    <figcaption id="captionBanner2">
+                        <h1>COLEÇÃO ATEMPORAL</h1>
+                        <p>Estilo e qualidade para durar.</p>
+                    </figcaption>
+                </picture>
+            </Transition>
+            <Transition :name="animateName">
+                <picture v-show="indexImg == 2">
+                    <source media="(max-width: 548px)" srcset="../assets/Mobile/BannerCarousel3.png">
+                    <source media="(max-width: 900px)" srcset="../assets/Tablet/BannerCarousel3.png">
+                    <img src="../assets/Desktop/BannerCarousel3.png" alt="Terceiro Banner">
+                    <figcaption id="captionBanner3">
+                        <h1>COLEÇÃO ATEMPORAL</h1>
+                        <p>Alto impacto visual, baixo impacto ambiental!</p>
+                    </figcaption>
+                </picture>
+            </Transition>
         </div>
         <div class="controlsButtons">
-            <button @click="prevImg" class=""><i class="uil uil-previous"></i></button>
-            <button @click="nextImg" class=""><i class="uil uil-step-forward"></i></button>
+            <button @click="prevImg" class="" :disabled="isAnimating"><i class="uil uil-previous"></i></button>
+            <button @click="nextImg" class="" :disabled="isAnimating"><i class="uil uil-step-forward"></i></button>
         </div>
         <div class="spotlightPosition">
-            <span :class="[indexImg == 0 ? 'active':'']" class="spotlightBar"></span>
-            <span :class="[indexImg == 1 ? 'active':'']" class="spotlightBar"></span>
-            <span :class="[indexImg == 2 ? 'active':'']" class="spotlightBar"></span>
+            <span :class="[ indexImg == 0 ? 'active animate__zoomIn':'']" class="spotlightBar animate__animated"></span>
+            <span :class="[indexImg == 1 ? 'active animate__zoomIn':'']" class="spotlightBar animate__animated"></span>
+            <span :class="[indexImg == 2 ? 'active animate__zoomIn':'']" class="spotlightBar  animate__animated"></span>
         </div>
     </div>
 </template>
@@ -43,31 +50,67 @@
 
     let intervalTimer: number | any = null
 
-    onMounted(() =>{startCarousel()})
+    let isAnimating = ref<boolean>(false)
 
-    onUnmounted(()=>{stopCarousel()})
+    let animateName = ref<string>("slide")
+
+    const handleVisibilityChange = () => {
+        if (document.visibilityState === "hidden") {
+            stopCarousel()
+        } else if (isAnimating.value) {
+            startCarousel()
+        }
+        
+    }
+    
+    onMounted(() =>{
+        document.addEventListener("visibilitychange", handleVisibilityChange)
+        startCarousel()
+    })
+
+    onUnmounted(()=>{
+        document.removeEventListener("visibilitychange", handleVisibilityChange)
+        stopCarousel()
+    })
 
     const startCarousel = () => {
        intervalTimer = setTimeout(nextImgWithTimeout,5000)
+       
     }
 
     const stopCarousel = () => {
         clearInterval(intervalTimer)
+        
     }
 
     const nextImg = () =>{
-        indexImg.value++
-        indexImg.value > 2 ? indexImg.value = 0:indexImg.value
+        animateName.value = "slide"
+        if(!isAnimating.value){
+            isAnimating.value = true
+            indexImg.value++
+            setTimeout(() => (isAnimating.value = false), 2000)
+            indexImg.value > 2 ? indexImg.value = 0:indexImg.value
+            
+        }
+        
     }
 
     const prevImg = () =>{
-        indexImg.value--
-        indexImg.value < 0 ? indexImg.value = 2:indexImg.value
+        animateName.value = "slideBack"
+        if(!isAnimating.value){
+            isAnimating.value = true
+            indexImg.value--
+            indexImg.value < 0 ? indexImg.value = 2:indexImg.value
+            setTimeout(() => (isAnimating.value = false), 2000)
+            
+        }
+        
     }
 
     const nextImgWithTimeout = () => {
         nextImg()
-        intervalTimer = setTimeout(nextImg,5000)
+        intervalTimer = setInterval(nextImg,5000)
+        
     }
 </script>
 
@@ -77,11 +120,15 @@
     }
 
     #carouselInner{
-        @apply relative w-full h-full overflow-hidden transform transition-transform duration-500
+        @apply relative w-full h-full overflow-hidden transition ease-in-out
+    }
+
+    picture{
+        @apply absolute top-0 right-0 left-0 bottom-0 w-full h-full
     }
 
     img{
-        @apply absolute top-0 right-0 left-0 bottom-0 w-full h-full
+        @apply h-full w-full
     }
 
     figcaption{
@@ -118,7 +165,7 @@
     }
 
     .controlsButtons > button{
-        @apply h-full w-8 text-3xl
+        @apply h-full w-8 text-3xl transition-all duration-500
     }
 
     .spotlightPosition{
@@ -130,5 +177,57 @@
 
     .active{
         @apply border-white
+    }
+    
+    button:disabled{
+        @apply text-gray-400
+    }
+
+    .slide-enter-active{
+        transition: all 2s linear;
+    }
+
+    .slide-enter-to{
+        transition: all 2s linear;
+        transform: translatex(0);
+    }
+
+    .slide-leave-active{
+        transition: all 2s linear;
+        transform: translateX(-100%)
+    }
+
+    .slide-enter-from{
+        transition: all 2s linear;
+        transform: translateX(100%)
+    }
+
+    .slide-leave-to{
+        transition: all 2s linear;
+        transform: translateX(-100%)
+    }
+
+    .slideBack-enter-active{
+        transition: all 2s linear;
+    }
+
+    .slideBack-enter-to{
+        transition: all 2s linear;
+        transform: translatex(0);
+    }
+
+    .slideBack-leave-active{
+        transition: all 2s linear;
+        transform: translateX(100%)
+    }
+
+    .slideBack-enter-from{
+        transition: all 2s linear;
+        transform: translateX(-100%)
+    }
+
+    .slideBack-leave-to{
+        transition: all 2s linear;
+        transform: translateX(100%)
     }
 </style>
